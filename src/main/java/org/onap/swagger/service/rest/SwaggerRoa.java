73b36b783e363b1eea326cc8a91be3d9111affba
Kanagaraj Manickam k00365106 <kanagaraj.manickam@huawei.com>
2018-09-17 14:17:14 +0530

    Improve CLM coverage
    Issue-ID: MSB-281
    
    Change-Id: Ibce48a398e62e92a0c6ba8040e73b0dd306a2234
    Signed-off-by: Kanagaraj Manickam k00365106 <kanagaraj.manickam@huawei.com>

--- src/main/java/org/onap/swagger/service/rest/SwaggerRoa.java
+++ src/main/java/org/onap/swagger/service/rest/SwaggerRoa.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Huawei Technologies Co., Ltd.
+ * Copyright 2017-18 Huawei Technologies Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,7 +35,7 @@ import javax.ws.rs.core.MediaType;
 public class SwaggerRoa {
 
     /**
-     * API doc.
+     * Swagger API doc.
      *
      * @return string
      * @throws IOException exception
